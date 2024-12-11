693e5d61ab74cd33f107ee3d8ddf8f601e3b354a
Axmed xassan Axmed <axmedxassana@gmail.com>
2024-12-11 19:08:38 +0300

    first commit

---
+++ Untitled-1.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>//header
+using namespace std;//library
+int main()//main function
+{
+ int a = 5;//1.declared variable a which stores 5
+ int b = 10;//2.declared variable b whivh stores 10;
+ int x;//3.declared empty variable called x
+ int z;//4. declared empty variable called z
+ cout << " HELLO MY DEAR FRIEND!" << endl;//5.displayin or outputting text
+ cout << "we are going to make a simple calculation below" << endl;//6.displaying or outputting string
+ cout << "--------------------------------------------------------" << endl;7.//displayin or outputting string
+ x = a + b;//8.storing x variable by addition of a and b variable
+ z=a*b;//9.storing z variable by a multiplied by b
+cout << "The current value of x after addition is = " << x << endl;//9.//displaying out text following x variable
+cout << "The current value of z after multiblication is = " << z << endl;//10.//displaying out text following z variable
+cout << "the programme is finished" << endl;//11.//displaying out text
+cout << "thank you" << endl;//12.displaying out text 
+}
---
+++ test2.cpp
@@ -0,0 +1,35 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    float q;
+     float w;
+      float t;
+       float e;
+        float r;
+        
+        cout<<"the sum will be desplayed only if it's more than or equal to 100 "<<endl;
+        cout<<"Enter the first number q";
+        cin>>q;
+        cout<<"enter second numn=ber w";
+        cin>>w;
+        cout<<"enter theid number e";
+        cin>>e;
+        
+        r=q+w+e;
+        t=q*w*e;
+        
+        if(r>=100){
+            cout<<"the current of r After addition is="<<r<<endl;
+            
+        }
+        cout<<"the current of t after muliblication is="<<t<<endl;
+}
+       
+        // else if(r==100){
+        //     cout<<"the sum is equal to 100; the sun="<<r<<endl;
+            
+        // }
+        // else{
+        //     cout<<"the"
+        // }
